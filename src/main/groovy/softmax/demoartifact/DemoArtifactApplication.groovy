f9e97ba3d1b34a68aa1258056c1a83f87364dc05
Anu-Kan <91402944+Anu-Kan@users.noreply.github.com>
2022-01-12 22:40:40 +0530

    artifacts created for softmax

---
+++ src/main/groovy/softmax/demoartifact/DemoArtifactApplication.groovy
@@ -0,0 +1,13 @@
+package softmax.demoartifact
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoArtifactApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DemoArtifactApplication, args)
+	}
+
+}
